55e213015a7c51546029982e0e8c1b277afcace5
Javi <suavejavi@gmail.com>
2020-05-05 08:40:04 -0400

    ⭐️ add mock list of heroes + display list with style

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero  } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Superman'},
+  {id: 12, name: 'Batman'},
+  {id: 13, name: 'Spider-Man'},
+  {id: 14, name: 'Green Lantern'},
+  {id: 15, name: 'Thor'},
+  {id: 16, name: 'Captain America'},
+  {id: 17, name: 'Martian Manhunter'},
+  {id: 18, name: 'Nightwing'},
+  {id: 19, name: 'Thing'},
+  {id: 20, name: 'Flash'},
+];
