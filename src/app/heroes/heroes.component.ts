fd992b7aa9e55c2fb1ac67d1d65abee6af50bfe6
Javi <suavejavi@gmail.com>
2020-05-11 07:55:06 -0400

    ⭐️ add HeroesComponent links and Dashboard link

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-import { Hero } from "../hero";
-import { HeroService } from "../hero.service";
-import { MessageService } from "../message.service";
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"],
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(
     private heroService: HeroService,
-    private messageService: MessageService
   ) {}
 
   ngOnInit() {
@@ -26,8 +23,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-  }
 }
