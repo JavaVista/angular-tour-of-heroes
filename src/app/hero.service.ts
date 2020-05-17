4a581271de57bb630b0bb3df850092f26e97e49f
Javi <suavejavi@gmail.com>
2020-05-17 14:30:52 -0400

    ⭐️ add error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,21 +11,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root',
 })
 export class HeroService {
+    private heroesUrl = 'api/heroes'; // URL to web api
 
-  private heroesUrl = 'api/heroes'; // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-  constructor(private http: HttpClient, private messageService: MessageService) {}
-
+  // Get heroes from the server
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    // this.messageService.add('HeroService: fetched heroes');
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes!')),
+        catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
+  }
+
+  /**
+   * Handle Http operation that failed. Let app continue.
+   * @param operation - name of operation that failed
+   * @param results - optional value to return as observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send error to remote logging infrastructure
+      console.log(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   // Log a HeroService message with MessageService
