52120b4f531fc52634406bc4830afbdf0731fcab
Javi <suavejavi@gmail.com>
2020-05-20 11:41:06 -0400

    ⭐️ add delete method to delete hero in the backend

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -76,4 +76,15 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+
+  // DELETE: delete hero from server
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
